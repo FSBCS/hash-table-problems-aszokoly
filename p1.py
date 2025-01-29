92bba7bdb5dd94d128e1e94e2b27e491afa06c65
aszokoly <154333598+aszokoly@users.noreply.github.com>
2025-01-29 20:29:54 +0000

    initial

--- p1.py
+++ p1.py
@@ -1,2 +1,15 @@
+
 def group_anagrams(words):
-    pass
+    anagrams = {}
+    for i in words:
+        values = ''.join(sorted(i))
+    
+        if values not in anagrams:
+            anagrams[values] = []
+
+        anagrams[values].append(i)
+    return list(anagrams.values())
+        
+words = ['tea', 'eat', 'tan', 'nat','bat']
+print(group_anagrams(words))
+    
--- p2.py
+++ p2.py
@@ -1,2 +1,19 @@
 def k_most_frequent(lst, k):
-    pass
+    values = {}
+
+    for i in lst:
+        if i not in values:
+            values[i] = 1
+        else:
+            values[i] += 1
+    
+    #lambda part is essentialy def key(x): return values[x]    
+    leaderboard = sorted(values.values(), key=lambda x:values[x], reverse=True)
+    leaderboard[k]
+
+lst = [1,2,1,1,2]
+print(k_most_frequent(lst, 2))
+
+
+
+        
