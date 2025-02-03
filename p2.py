765bd41b3d5a77ac71b5a2a318a320275f7c4c0f
aszokoly <154333598+aszokoly@users.noreply.github.com>
2025-02-03 17:56:06 +0000

    can you look at p2 please

--- p2.py
+++ p2.py
@@ -8,11 +8,19 @@ def k_most_frequent(lst, k):
             values[i] += 1
     
     #lambda part is essentialy def key(x): return values[x]    
-    leaderboard = sorted(values.values(), key=lambda x:values[x], reverse=True)
-    leaderboard[k]
+    leaderboard = sorted(values.keys(), key=lambda x: values[x], reverse=True)
+    leaderboard[k - 1]
+    
+    while k > 1:
+        k -= 1
+        leaderboard[k]
+    
+    print(leaderboard)
 
-lst = [1,2,1,1,2]
-print(k_most_frequent(lst, 2))
+lst = [1,2,1,3,1,2]
+k_most_frequent(lst, 2)
+
+
 
 
 
